be156eae36ad94745f4df1209aa72ebad2930fa6
R Harikrishnan <128880349+harikrishnan669@users.noreply.github.com>
2025-10-28 20:53:30 +0530

    Update targcodegen.c

--- targcodegen.c
+++ targcodegen.c
@@ -1,43 +1,50 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
-#define LEN 5
+#define LEN 4
 #define SIZE 50
+
 struct
 {
-    char res[LEN];
-    char op;
-    char opnd1[LEN];
-    char opnd2[LEN];
-} tac[SIZE]; //To store three address code
-int num_instr=0;
+        char res[LEN];
+        char op;
+        char opnd1[LEN];
+        char opnd2[LEN];
+}tac[SIZE];
+
+int num,i;
 char instr[SIZE];
-void read_three_address_code()
+
+void three_address_code()
 {
-    printf("Enter the number of instructions\n");
-    scanf("%d",&num_instr);
-    printf("Enter each instruction, seperate result opcodes and operands by space\n");
-    printf("E.g. s = a + b\n");
-    for(int i=0;i<num_instr;i++)
-    {
-        scanf("%s = %s %c %s",&tac[i].res,&tac[i].opnd1,&tac[i].op,&tac[i].opnd2);
-    }
+printf("Enter the number of instruction:");
+scanf("%d",&num);
+printf("Enter each instruction as Eg: s = a + b\n");
+for(i=0;i<num;i++)
+{
+        scanf("%s = %s %c %s",tac[i].res,tac[i].opnd1,&tac[i].op,tac[i].opnd2);
 }
+}
+
 char *get_instr(char op)
-{   //ASCII values are * = 42,+ = 43, - = 45 and / = 47
-    char mnemonics[][LEN]={"MUL","ADD","","SUB","","DIV"};
-    strcpy(instr,mnemonics[op-'*']);
-    return instr;
-} 
-void generate_target_code()
 {
-    for(int i=0;i<num_instr;i++)
-    {
-        printf("MOV AX,%s\n%s AX,%s\nMOV %s,AX\n",tac[i].opnd1,get_instr(tac[i].op),tac[i].opnd2,tac[i].res);
-    }
+char mnemonics[][LEN]={"MUL","ADD","","SUB","","DIV"};
+strcpy(instr,mnemonics[op-'*']);
+return instr;
 }
+
+
+void generate_three_addresscode()
+{
+for(i=0;i<num;i++)
+{
+        printf("MOV AX,%s\n%s AX,%s\nMOV %s,AX\n",tac[i].opnd1,get_instr(tac[i].op),tac[i].opnd2,tac[i].res);
+}
+}
+
 int main()
 {
-    read_three_address_code();
-    printf("Equivalent Assembly code is:\n");
-    generate_target_code();
+three_address_code();
+printf("Equivalent assembly code is:\n");
+generate_three_addresscode();
+}
