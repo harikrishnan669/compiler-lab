b3c6cbaf1fe86807f8836e78b16fc8597fbb53a6
R Harikrishnan <128880349+harikrishnan669@users.noreply.github.com>
2025-10-29 15:03:55 +0530

    Update cf.c

--- cf.c
+++ cf.c
@@ -1,91 +1,64 @@
 #include<stdio.h>
-
 #include<string.h>
-
 #include<ctype.h>
 
-void input();
-void output();
-void change(int p, char * res);
-void constant();
+#define MAX 50
 
-struct expr {
-  char op[2], op1[5], op2[5], res[5];
-  int flag;
-}
-arr[10];
-int n;
-void main() {
+char operands[MAX][10];
+char operators[MAX];
+int topOpnd = -1, topOp = -1;
 
-  input();
-  constant();
-  output();
+int precedence(char c) {
+        if(c == '+' || c == '-')
+                return 1;
+        if(c == '*' || c == '/')
+                return 2;
+        if(c == '=')
+                return -1;
+        return 0;
 }
 
-void input() {
+void generateICG(char exp[]) {
+        int i = 0, t = 1;
+        char temp[10];
 
-  int i;
-  printf("\n\nEnter the maximum number of expressions : ");
-  scanf("%d", & n);
-  printf("\nEnter the input : \n");
-  for (i = 0; i < n; i++) {
-    scanf("%s", arr[i].op);
-    scanf("%s", arr[i].op1);
-    scanf("%s", arr[i].op2);
-    scanf("%s", arr[i].res);
-    arr[i].flag = 0;
-  }
+        while(exp[i] != '\0') {
+                if(isalnum(exp[i])) {
+                        int j = 0;
+                        while(isalnum(exp[i])) temp[j++] = exp[i++];
+                        temp[j] = '\0';
+                        strcpy(operands[++topOpnd],temp);
+                }
+                else {
+                        while(topOp >= 0 && precedence(operators[topOp]) >= precedence(exp[i])) {
+                                char op = operators[topOp--];
+                                if(op == '=') {
+                                        printf("%s = %s\n",operands[topOpnd-1],operands[topOpnd]);
+                                        topOpnd -= 2;
+                                }
+                                else {
+                                        printf("t%d = %s %c %s\n",t,operands[topOpnd-1],op,operands[topOpnd]);
+                                        sprintf(operands[--topOpnd],"t%d",t++);
+                                }
+                        }
+                        operators[++topOp] = exp[i++];
+                }
+        }
+        while(topOp >= 0) {
+                char op = operators[topOp--];
+                if(op == '=')
+                        printf("%s = %s\n",operands[topOpnd-1],operands[topOpnd]);
+                else {
+                        printf("t%d = %s %c %s\n",t,operands[topOpnd-1],op,operands[topOpnd]);
+                        sprintf(operands[--topOpnd],"t%d",t++);
+                }
+        }
 }
 
-void constant() {
-  int i;
-  int op1, op2, res;
-  char op, res1[5];
-  for (i = 0; i < n; i++) {
-    if (isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0]) || strcmp(arr[i].op, "=") == 0) {
-      /*if both digits, store them in variables*/
-      op1 = atoi(arr[i].op1);
-      op2 = atoi(arr[i].op2);
-      op = arr[i].op[0];
-      switch (op) {
-      case '+':
-        res = op1 + op2;
-        break;
-      case '-':
-        res = op1 - op2;
-        break;
-      case '*':
-        res = op1 * op2;
-        break;
-      case '/':
-        res = op1 / op2;
-        break;
-      case '=':
-        res = op1;
-        break;
-      }
-      sprintf(res1, "%d", res);
-      arr[i].flag = 1;
-      change(i, res1);
-    }
-  }
-}
-
-void output() {
-  int i = 0;
-  printf("\nOptimized code is : ");
-  for (i = 0; i < n; i++) {
-    if (!arr[i].flag)
-      printf("\n%s %s %s %s", arr[i].op, arr[i].op1, arr[i].op2, arr[i].res);
-  }
-}
-
-void change(int p, char * res) {
-  int i;
-  for (i = p + 1; i < n; i++) {
-    if (strcmp(arr[p].res, arr[i].op1) == 0)
-      strcpy(arr[i].op1, res);
-    else if (strcmp(arr[p].res, arr[i].op2) == 0)
-      strcpy(arr[i].op2, res);
-  }
+int main() {
+        char exp[MAX];
+        printf("Enter infix expression without space: ");
+        scanf("%[^\n]",exp);
+        generateICG(exp);
+        return 0;
 }
